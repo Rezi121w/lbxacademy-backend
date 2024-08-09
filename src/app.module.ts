58900ad8d231c35395aa40dadc041c41cf15d13b
Rezi <reziabulashvili@gmail.com>
2024-08-09 17:35:30 +0400

    added timezone

--- src/app.module.ts
+++ src/app.module.ts
@@ -25,6 +25,7 @@ import { TimersModule } from './timers/timers.module';
       username: process.env.MYSQL_USER,
       password: process.env.MYSQL_PASS,
       database: process.env.MYSQL_DBNAME,
+      timezone: 'Z',
       autoLoadEntities: true,
       synchronize: true, // Only For Dev Mode //
     }),
